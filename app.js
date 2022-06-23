f15150092c71fbf4f2ef77e510a5002163346c3f
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-23 22:16:48 +0800

    [update] init app and index

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const express = require("express");
+const app = express();
+
+
+app.get("/", function(req, res) {
+
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+});
