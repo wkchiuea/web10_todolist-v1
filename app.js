dc5ac8143ab1e1d6a1773d813a7c03f2d42192c7
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-23 22:40:19 +0800

    [update] run code inside ejs template

--- app.js
+++ app.js
@@ -2,9 +2,45 @@
 
 const express = require("express");
 const app = express();
-
+app.use("view engine", ejs); // set view engine to use ejs
 
 app.get("/", function(req, res) {
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tueday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            break;
+    }
+
+    // assume views directory exist and containing list.ejs
+    // use render instead of sendFile
+    res.render("list", {
+        kindOfDay: day
+    });
 
 });
 
